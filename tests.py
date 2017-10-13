40647dc3e496731f2475d5b24603bd1ca8342cba
lucky <lucky_is@mail.ru>
2017-10-13 12:18:38 +0300

    first test

--- tests.py
+++ tests.py
@@ -1,1 +1,13 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price, get_thousands
+
+
+class FormattingPriceTestCase(unittest.TestCase):
+    def test_get_thousands(self):
+        got_list = get_thousands(123456789)
+        check_list = [789, 456, 123]
+        self.assertListEqual(check_list, check_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
