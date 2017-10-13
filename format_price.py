299cd68cc26b19c057cecbec02833d848e4ecbaa
lucky <lucky_is@mail.ru>
2017-10-13 12:09:50 +0300

    first version

--- format_price.py
+++ format_price.py
@@ -1,5 +1,26 @@
+import math
+
+
+def get_thousands(int_value):
+    thousands = []
+    i = 0
+    while int_value:
+        thousands.append(int(int_value % 1000))
+        int_value = int_value // 1000
+    return thousands
+
+
 def format_price(price):
-    pass
+    try:
+        (fract, int_value) = math.modf(price)
+        thousands = get_thousands(int_value)
+        if round(fract, 2) > 0:
+            thousands[0] += round(fract, 2)
+        return ' '.join(map(str, reversed(thousands)))
+    except TypeError as e:
+        print(str(e))
+
+
 
 if __name__ == '__main__':
-    pass
+    print(format_price(1555323444.234))
